84106f86e62a7547e5f1c6758be8bf9e15d13b79
Jorge Sanchez <jorjatorz@gmail.com>
2023-03-27 21:49:11 +0200

    UI changes

--- src/pages/book/[id].js
+++ src/pages/book/[id].js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
+import "../../globals.css";
+
 // DEBUG
 import data from '../../../public/sample.json'
 
@@ -10,16 +12,38 @@ export function getBookData(id) {
 
 
 export default function BookPage({ book }) {
-    console.log(book)
     return (
-        <main>
-            <div className='bg-amber-300 w-full h-screen m-auto'>
-                <div className='flex justify-center'>
-                    <h3>{book.title}</h3>
-                    <Image src={book.image_compressed} width="300" height="200" alt="Picture of the book cover" />
+        <main className='bg-amber-300 w-full h-full m-auto'>
+            <div className='flex px-32 py-20 gap-16'>
+                <div className='shrink-0'>
+                    <Image src={book.image} width="350" height="0" alt="Picture of the book cover" />
+                </div>
+                <div className='grow'>
+                    <h1 className='text-6xl font-bold'>{book.title}</h1>
+                    <h2 className='text-2xl italic'>by <span className='font-bold'>{book.author}</span></h2>
+                    <ul className='flex gap-4 mt-2'>{book.topics.map((topic) =>
+                        <li className='bg-orange-500 py-1 px-2 rounded-lg'>{topic}</li>)}
+                    </ul>
+                    <div className='mt-8'>{book.description}</div>
+                    <div>
+                        <h2 className='text-4xl font-bold mt-4'>Key Ideas</h2>
+                        <ul>{book.key_ideas.map((idea) =>
+
+                            <li className='w-100% min-h-[12rem] rounded-md bg-slate-200 my-6 shadow-md p-4'>
+                                <h3 className='text-xl font-bold italic'>{idea.title}</h3>
+                                <p className='text-lg mt-4'>{idea.description}</p>
+                            </li>
+                        )}
+                        </ul>
+                    </div>
+                </div>
+                <div className='basis-1/4'>
+                    <h2 className='text-2xl text-center'>Related books</h2>
+                    {book.similar_books.map((sb) =>
+                        <div className='bg-red-300 w-[16rem] h-[22rem] my-8 py-4 text-center'>{sb.title}</div>)}
                 </div>
             </div>
-        </main>
+        </main >
     )
 }
 
