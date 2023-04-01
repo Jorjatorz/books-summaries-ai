0ba3de1a7f1e305a352a62eb41bb112af5d88e3b
Jorge Sanchez <jorjatorz@gmail.com>
2023-04-02 00:28:29 +0200

    Changes to UI

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,9 +4,13 @@ import { BsSearch } from 'react-icons/bs'
 export default class SearchBar extends Component {
     render() {
         return (
-            <div className='relative w-[40%]'>
-                <input className='w-full p-4 rounded-md shadow-md' placeholder='Enter your book title' />
-                <BsSearch className='absolute right-4 bottom-1/2 translate-y-1/2 text-emerald-500 font-bold' size={25} />
+            <div className={this.props.className}>
+                <div className='focus-within:opacity-100 relative w-full p-3 rounded-full shadow-md opacity-80 bg-white'>
+                    <div className='flex gap-2'>
+                        <BsSearch className='text-[#373738]' size={25} />
+                        <input className='w-full outline-none border-none' placeholder='Enter your book title' />
+                    </div>
+                </div>
             </div>
         )
     }
