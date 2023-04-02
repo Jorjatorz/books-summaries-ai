bde82d9dafcc04c00ae84722b223b5a6870e26a7
Jorge Sanchez <jorjatorz@gmail.com>
2023-04-02 18:36:24 +0200

    UI chanfes

--- src/pages/book/[slug].js
+++ src/pages/book/[slug].js
@@ -2,12 +2,12 @@ import { React, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
+import SearchBar from '@/components/search_bar';
 
 import "../../globals.css";
 
 // DEBUG
 import data from '../../../public/sample.json'
-import SearchBar from '@/components/search_bar';
 
 export function getBookData(slug) {
     return data[slug];
@@ -33,10 +33,10 @@ function KeyIdeas(props) {
     return (
         <div>
             <ul>{book.key_ideas.map((idea) =>
-                <li className='relative flex flex-col justify-center w-100% min-h-[12rem] rounded-md text-center bg-white my-6 shadow-md p-4 hover:scale-[1.02] ease-in duration-300'>
+                <li className='flex flex-col justify-center w-100% min-h-[12rem] rounded-md text-center bg-white my-6 shadow-md p-4 hover:scale-[1.02] ease-in duration-300'>
+                    <AIToolTip className='flex justify-end' />
                     <h3 className='text-2xl font-bold italic'>{idea.title}</h3>
                     <p className='text-lg mt-4'>{idea.description}</p>
-                    <AIToolTip className='absolute right-0 top-0 m-4' />
                 </li>
             )}
             </ul>
@@ -49,10 +49,10 @@ function ChaperSummaries(props) {
     return (
         <div>
             <ul>{book.chapters.map((chapter) =>
-                <li className='relative flex flex-col justify-center w-100% min-h-[12rem] rounded-md text-center bg-white my-6 shadow-md p-4 hover:scale-[1.02] ease-in duration-300'>
+                <li className='flex flex-col justify-center w-100% min-h-[12rem] rounded-md text-center bg-white my-6 shadow-md p-4 hover:scale-[1.02] ease-in duration-300'>
+                    <AIToolTip className='flex justify-end' />
                     <h3 className='text-2xl font-bold italic'>{chapter.title}</h3>
                     <p className='text-lg mt-4'>{chapter.description}</p>
-                    <AIToolTip className='absolute right-0 top-0 m-4' />
                 </li>
             )}
             </ul>
@@ -69,7 +69,7 @@ function ValueAddedTabs(props) {
         <div>
             <ul className='flex items-center justify-center text-center m-auto gap-8'>
                 {titles.map((title, idx) =>
-                    <li className={`min-w-[12rem] text-lg font-bold ${idx === index ? "opacity-100" : "opacity-70"} bg-[#373738] text-white rounded-lg p-2 cursor-pointer hover:scale-105 duration-150`} onClick={() => setIndex(idx)}>{title}</li>
+                    <li className={`min-w-[12rem] text-lg font-bold ${idx === index ? "opacity-100" : "opacity-70"} bg-[#4A4A4A] text-white rounded-lg p-2 cursor-pointer hover:scale-105 duration-150`} onClick={() => setIndex(idx)}>{title}</li>
                 )}
             </ul>
             {bodies[index]}
@@ -80,39 +80,43 @@ function ValueAddedTabs(props) {
 
 export default function BookPage({ book }) {
     return (
-        <main className={`bg-gradient-to-b from-[#76a4ca] to-[24rem] to-[#e7e7e9] w-full h-full m-auto pt-4`}>
+        <main className={`bg-gradient-to-b from-[#76a4ca] to-[24rem] to-[#F0EFE2] w-full h-full m-auto pt-4`}>
             <div className='flex items-center text-center justify-center m-auto'><SearchBar className='w-[40%]' /></div>
             <div className='flex pl-32 py-12 gap-16'>
-                <div className='shrink-0'>
+                <div className='shrink-0 max-w-[200px]'>
                     <div className={`bg-gray-50 p-4 rounded-lg shadow-xl`}>
-                        <Image src={book.image} width="350" height="0" alt="Picture of the book cover" />
+                        <Image src={book.image} width="200" height="0" alt="Picture of the book cover" />
                     </div>
-                    <Link href={book.amazon_url}>
-                        <h2 className='text-2xl font-bold text-center m-2 text-[#158582]'>Support the Author <br /> Buy it!</h2>
+                    <Link href={book.amazon_url} target='_blank'>
+                        <h2 className='text-2xl font-bold text-center m-2 text-[#4A4A4A]'>Support the Author <br /> Buy it!</h2>
                     </Link>
                 </div>
                 <div className='grow'>
-                    <h1 className='text-6xl font-bold'>{book.title}</h1>
+                    <h1 className='text-5xl font-bold'>{book.title}</h1>
                     <h2 className='text-2xl italic'>by <span className='font-bold'>{book.author}</span></h2>
                     <ul className='flex gap-4 mt-2'>{book.topics.map((topic) =>
-                        <li className='bg-[#F4E6CB] py-1 px-2 rounded-lg font-medium'>{topic}</li>)}
+                        <li className='bg-[#B9F2D8] py-1 px-2 rounded-lg font-medium'>{topic}</li>)}
                     </ul>
                     <div className='my-8 text-lg'>{book.description}</div>
                     <ValueAddedTabs titles={["Key Ideas", "Chapter Summaries"]} bodies={[<KeyIdeas book={book} />, <ChaperSummaries book={book} />]} />
 
 
                 </div>
-                <div className='basis-1/4'>
-                    <h2 className='text-4xl text-center font-bold'>Related books</h2>
+                <div className='basis-1/4 mr-4'>
+                    <h2 className='text-2xl text-center font-semibold'>More like this</h2>
+                    <ul className='flex flex-col gap-8 mt-4'>
                     {book.similar_books.map((sb) =>
                         <Link href={`book/${sb.slug}`}>
-                            <div className='p-2 mx-2 my-4 bg-white rounded-lg hover:scale-105 ease-in duration-100'>
-                                <div className='relative flex flex-col w-[12rem] h-[18rem] m-auto text-center'>
-                                    <Image src={sb.image} fill alt={`Picture of a related book with name ${sb.title}`} />
+                            <li>
+                                <div className='p-2 bg-white rounded-lg hover:scale-105 ease-in duration-100'>
+                                    <div className='relative flex flex-col h-[150px] w-[100px] m-auto text-center'>
+                                        <Image src={sb.image} fill alt={`Picture of a related book with name ${sb.title}`} />
+                                    </div>
                                 </div>
-                            </div>
+                            </li>
                         </Link>
                     )}
+                    </ul>
                 </div>
             </div>
         </main >
