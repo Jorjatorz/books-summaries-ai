163297a1688e10944c2b76c8d98fab807c0e165a
jorjatorz <jorjatorz@gmail.com>
2023-07-10 00:04:05 +0200

    added env variables

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    REACT_APP_CONTENTFUL_SPACE_ID: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+    REACT_APP_CONTENTFUL_ACCESS_TOKEN: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+  }
 }
 
 module.exports = nextConfig
